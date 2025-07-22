27d913e7a899435a4e08711fb3d55dfea56af65b
Charbel Toumieh <charbel.toumieh@epfl.ch>
2025-07-22 16:02:36 +0200

    set planning active to false by default

--- multi_agent_planner/include/multi_agent_planner/agent_class.hpp
+++ multi_agent_planner/include/multi_agent_planner/agent_class.hpp
@@ -417,7 +417,7 @@ private:
   // planning is active when this variable is true; set by default to true if
   // you want the initial state to be the one in the launch file and to start
   // planning immediately
-  bool planning_active_ = true;
+  bool planning_active_ = false;
 
   /* yaw control variables */
   // current yaw angle
