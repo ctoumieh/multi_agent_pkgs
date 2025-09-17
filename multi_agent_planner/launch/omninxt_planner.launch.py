a9c4abd3540fbf7e8432cf41c6eade3524e46dff
Niel Mistry <niel@nielmistry.com>
2025-09-17 15:42:44 +0200

    Revert "don't spam rcl"
    This reverts commit 07dc6a6a107ef34547e19f7779993cd5e58a19ed.

--- multi_agent_planner/launch/omninxt_planner.launch.py
+++ multi_agent_planner/launch/omninxt_planner.launch.py
@@ -63,7 +63,7 @@ def launch_nodes(context, *args, **kwargs):
             ],
             # prefix=['xterm -fa default -fs 10 -xrm "XTerm*selectToClipboard: true" -e gdb -ex run --args'],
             # prefix=['xterm -fa default -fs 10 -hold -e'],
-            arguments=['--ros-args', '--log-level', 'your_node:=DEBUG', '--log-level', 'rcl:=INFO'],
+            arguments=['--ros-args', '--log-level', 'DEBUG'],
             output='screen',
             emulate_tty=True,
         )
@@ -86,7 +86,7 @@ def launch_nodes(context, *args, **kwargs):
         ],
         # prefix=['xterm -fa default -fs 10 -xrm "XTerm*selectToClipboard: true" -e gdb -ex run --args'],
         # prefix=['xterm -fa default -fs 10 -hold -e'],
-        arguments=['--ros-args', '--log-level', 'your_node:=DEBUG', '--log-level', 'rcl:=INFO'],
+        arguments=['--ros-args', '--log-level', 'DEBUG'],
         output='screen',
         emulate_tty=True,
     )
