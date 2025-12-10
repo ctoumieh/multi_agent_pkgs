52f407ac349fc8bc8ac4d94a39512490b5456cf0
Charbel Toumieh <charbel.toumieh@epfl.ch>
2025-12-10 23:39:22 +0100

    update mapping

--- mapping_util/src/map_builder.cpp
+++ mapping_util/src/map_builder.cpp
@@ -824,9 +824,9 @@ void MapBuilder::ClearLine(::voxel_grid_util::VoxelGrid &vg_curr,
       ::Eigen::Vector3d z_b = rot_mat.col(2);
       ::Eigen::Vector3d cam_position = cam_pose.translation();
       ::Eigen::Vector3d dir = end - cam_position;
-      ::Eigen::Vector3d dir_xz = dir - dir.dot(y_b) * y_b; dir_xz.normalize();
-      ::Eigen::Vector3d dir_xy = dir - dir.dot(z_b) * z_b; dir_xy.normalize();
-      if (dir_xy.dot(x_b) > cos(cam_info.hfov_radians / 2.0) && dir_xz.dot(x_b) > cos(cam_info.vfov_radians / 2.0)) {
+      ::Eigen::Vector3d dir_zx = dir - dir.dot(y_b) * y_b; dir_zx.normalize();
+      ::Eigen::Vector3d dir_zy = dir - dir.dot(x_b) * z_b; dir_zy.normalize();
+      if (dir_zy.dot(x_b) > cos(cam_info.vfov_radians / 2.0) && dir_zx.dot(x_b) > cos(cam_info.hfov_radians / 2.0)) {
         in_fov = true; start_f = cam_position; break;
       }
   }
